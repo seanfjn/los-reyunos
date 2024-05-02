994012bb14eb799171da076627f03daa190b662a
MegaBrett <forjan017@gmail.com>
2024-05-02 12:22:09 -0400

    Adding Images + Editing Navbar

--- Navbar Pages/Imagenes/imagenes.js
+++ Navbar Pages/Imagenes/imagenes.js
@@ -1,4 +1,4 @@
-filterSelection("all")
+filterSelection("all");
 function filterSelection(c) {
   var x, i;
   x = document.getElementsByClassName("filterDiv");
@@ -46,107 +46,74 @@ for (var i = 0; i < btns.length; i++) {
   });
 }
 
-var imageUrls = ['url("./Gallery/Images/apart_patio.jpg")']
+var imageUrls = ['url("./Gallery/Images/apart_patio.jpeg")'];
 
 let vid = document.getElementsByClassName("myVideo");
-vid.playbackRate = 0.40;
+vid.playbackRate = 0.4;
 
-
-var imageNumber = imageUrls.length
-
+var imageNumber = imageUrls.length;
 
 function imagePop(id) {
+  document.getElementById("imagePopId").style.display = "block";
+  document.getElementsByTagName("body")[0].style.overflowY = "hidden";
 
+  var imageName = document.getElementById(id + "-image").style.backgroundImage;
 
-  document.getElementById('imagePopId').style.display = 'block'
-  document.getElementsByTagName('body')[0].style.overflowY = 'hidden'
+  var imageIndex = 0;
 
-  var imageName = document.getElementById(id + '-image').style.backgroundImage
+  imageIndex = imageUrls.indexOf(imageName);
 
-  var imageIndex = 0
+  document.getElementsByClassName("imageContain")[0].style.animation =
+    "he 800ms forwards";
 
-  imageIndex = imageUrls.indexOf(imageName)
-
-  document.getElementsByClassName('imageContain')[0].style.animation = 'he 800ms forwards'
-
-  document.getElementsByClassName('imageContain')[0].style.backgroundImage = imageUrls[imageIndex]
+  document.getElementsByClassName("imageContain")[0].style.backgroundImage =
+    imageUrls[imageIndex];
 }
-
-
 
 function imageChange(id) {
+  var imageName = document.getElementById(id).style.backgroundImage;
 
-  var imageName = document.getElementById(id).style.backgroundImage
+  var imageIndex = 0;
 
-  var imageIndex = 0
+  imageIndex = imageUrls.indexOf(imageName);
 
-  imageIndex = imageUrls.indexOf(imageName)
+  if (imageIndex >= imageNumber - 1) imageIndex = 0;
+  else imageIndex++;
 
-  if (imageIndex >= imageNumber - 1)
-    imageIndex = 0
-  else
-    imageIndex++
-
-
-  document.getElementById(id).style.backgroundImage = imageUrls[imageIndex]
-
-
-
-
+  document.getElementById(id).style.backgroundImage = imageUrls[imageIndex];
 }
-
-
 
 function imageMoveLeft() {
+  var imageName =
+    document.getElementById("imageContainId").style.backgroundImage;
 
-  var imageName = document.getElementById('imageContainId').style.backgroundImage
+  var imageIndex = 0;
 
-  var imageIndex = 0
+  imageIndex = imageUrls.indexOf(imageName);
 
-  imageIndex = imageUrls.indexOf(imageName)
+  if (imageIndex <= 0) imageIndex = imageNumber - 1;
+  else imageIndex--;
 
-  if (imageIndex <= 0)
-    imageIndex = imageNumber - 1
-  else
-    imageIndex--
-
-
-  document.getElementById('imageContainId').style.backgroundImage = imageUrls[imageIndex]
-
-
-
-
+  document.getElementById("imageContainId").style.backgroundImage =
+    imageUrls[imageIndex];
 }
-
-
 
 function imageMoveRight() {
+  var imageName =
+    document.getElementById("imageContainId").style.backgroundImage;
 
-  var imageName = document.getElementById('imageContainId').style.backgroundImage
+  var imageIndex = 0;
 
-  var imageIndex = 0
+  imageIndex = imageUrls.indexOf(imageName);
 
-  imageIndex = imageUrls.indexOf(imageName)
+  if (imageIndex >= imageNumber - 1) imageIndex = 0;
+  else imageIndex++;
 
-  if (imageIndex >= imageNumber - 1)
-    imageIndex = 0
-  else
-    imageIndex++
-
-
-  document.getElementById('imageContainId').style.backgroundImage = imageUrls[imageIndex]
-
-
-
-
+  document.getElementById("imageContainId").style.backgroundImage =
+    imageUrls[imageIndex];
 }
 
-
-
-
 function imagePopNone() {
-
-  document.getElementsByClassName('imagePop')[0].style.display = 'none'
-  document.getElementsByTagName('body')[0].style.overflowY = 'scroll'
-
+  document.getElementsByClassName("imagePop")[0].style.display = "none";
+  document.getElementsByTagName("body")[0].style.overflowY = "scroll";
 }
